ff0e3ef700416887a291c8adfd5d801e3eecb7b3
Rinch WU <whf535802393@gmail.com>
2024-01-05 21:35:31 +0800

    VERSION = (1, 0, 3)

--- push.sh
+++ push.sh
@@ -2,3 +2,7 @@
 git add ./rinch_sql/__version__.py
 git commit -m"$(head -n 1 rinch_sql/__version__.py)"
 git push
+
+git add .
+git commit -m"$(head -n 1 rinch_sql/__version__.py)"
+git push
--- rinch_sql/__init__.py
+++ rinch_sql/__init__.py
@@ -2,4 +2,5 @@ from .__version__ import __version__
 from .sql import Sql
 from .sql_static import SqlStatic
 from .mysql import Mysql
+from .table import Table
 from mysql.connector.errors import IntegrityError
