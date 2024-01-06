0cac01207da98a6c31afc94cd8450be950aca5c4
Rinch WU <whf535802393@gmail.com>
2024-01-06 23:53:11 +0800

    VERSION = (1, 0, 7)

--- rinch_sql/table.py
+++ rinch_sql/table.py
@@ -3,9 +3,10 @@ from .mysql import Mysql
 
 class Table:
     db_config: dict[str] = None
+    field_list_unique: list[str]
 
     @classmethod
     def db(cls, db_config: dict[str] = None):
-        db_config = db_config or Table.db_config or cls.db_config
+        db_config = db_config or cls.db_config
         db = Mysql(db_config, cls)
         return db
