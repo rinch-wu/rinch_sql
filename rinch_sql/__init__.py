d7a106b87d6644f9184da4f737974329504d0b27
Rinch WU <whf535802393@gmail.com>
2024-01-07 16:07:45 +0800

    VERSION = (1, 3, 0)

--- rinch_sql/__init__.py
+++ rinch_sql/__init__.py
@@ -1,6 +1,8 @@
 from .__version__ import __version__
-from .sql import Sql
 from .sql_static import SqlStatic
+from .sql import Sql
 from .mysql import Mysql
+
+from .db_config import DbConfig
 from .table import Table
 from mysql.connector.errors import IntegrityError
--- rinch_sql/__version__.py
+++ rinch_sql/__version__.py
@@ -1,4 +1,4 @@
-VERSION = (1, 2, 1)
+VERSION = (1, 3, 0)
 
 __version__ = ".".join(map(str, VERSION))
 
