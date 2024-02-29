dfab6c27fc994d8d3088b936d1ce38aa36a16d60
Rinch WU <whf535802393@gmail.com>
2024-02-29 17:32:11 +0800

    VERSION = (8, 2, 0)

--- rinch_sql/use_var.py
+++ rinch_sql/use_var.py
@@ -1,10 +1,21 @@
-from dataclasses import dataclass as dataclass_
 from dataclasses import replace
 from datetime import date, datetime
 
 
 def dataclass(cls):
-    cls = dataclass_(cls)
-    cls.__eq__ = cls.__eq2__
-    cls.__hash__ = cls.__hash2__
-    return cls
+    from dataclasses import dataclass as dataclass_
+    from functools import wraps
+
+    @wraps(cls)
+    @dataclass_
+    class DecoratedClass(cls):
+        def __hash__(self):
+            return hash(tuple(self))
+
+        def __eq__(self, other):
+            if isinstance(other, self.__class__):
+                return tuple(self) == tuple(other)
+            else:
+                return False
+
+    return DecoratedClass
